eec220cdfd1e7122efd230765e59d26d22dc1f49
DucksAreMammals <34693858+DucksAreMammals@users.noreply.github.com>
2022-04-07 01:30:20 -0500

    Added first person controller

---
+++ source/Scripts/Player.gd
@@ -0,0 +1,42 @@
+extends KinematicBody
+
+var mouse_sensitivity := 0.45
+
+var acceleration_speed := 3.0
+
+var friction := 0.5
+var gravity := 8.0
+
+var velocity := Vector3.ZERO
+
+func _process(delta):
+	_apply_gravity()
+	_apply_movement()
+	
+	velocity = move_and_slide(velocity, Vector3.UP, true)
+
+func _input(event):
+	if event is InputEventMouseMotion and Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		rotation_degrees.y += -event.relative.x * mouse_sensitivity
+		$Camera.rotation_degrees.x = clamp($Camera.rotation_degrees.x + -event.relative.y * mouse_sensitivity, -90, 90)
+
+
+func _apply_gravity():
+	velocity.y -= gravity
+	velocity *= friction
+
+
+func _apply_movement():
+	var back = 1 if Input.is_action_pressed("Back") else 0
+	var forward = 1 if Input.is_action_pressed("Forward") else 0
+	var left = 1 if Input.is_action_pressed("Left") else 0
+	var right = 1 if Input.is_action_pressed("Right") else 0
+	
+	var acceleration = Vector2(forward - back, left - right).rotated(deg2rad(rotation_degrees.y))
+	
+	if acceleration.length_squared() > 1:
+		acceleration *= 1 / acceleration.length()
+	
+	velocity.x += acceleration.x * acceleration_speed
+	velocity.z += -acceleration.y * acceleration_speed
+	
