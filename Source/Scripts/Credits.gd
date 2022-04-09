a5ce857b1740f6d72314cb9b8754103a9e7bf93d
DucksAreMammals <34693858+DucksAreMammals@users.noreply.github.com>
2022-04-08 22:52:08 -0500

    Added cat render and fixed credits

--- Source/Scripts/Credits.gd
+++ Source/Scripts/Credits.gd
@@ -4,5 +4,6 @@ func _on_RichTextLabel_meta_clicked(meta):
 	OS.shell_open(str(meta))
 
 func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 	yield(get_tree().create_timer(5.0), "timeout")
 	visible = true
