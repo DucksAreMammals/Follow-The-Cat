480d1dc3e3cc9eaeba118f07638876b48ad2f03b
DucksAreMammals <34693858+DucksAreMammals@users.noreply.github.com>
2022-04-08 21:35:05 -0500

    Fixed sound playing

--- Source/Scripts/Game.gd
+++ Source/Scripts/Game.gd
@@ -2,3 +2,7 @@ extends Spatial
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	OS.window_fullscreen = true
+
+func _load_next():
+	pass
--- Source/Scripts/KurtisIsHereArea.gd
+++ Source/Scripts/KurtisIsHereArea.gd
@@ -4,5 +4,5 @@ var already_played := false
 
 func _on_KurtisIsHereArea_body_entered(body):
 	if not already_played:
-		$AnimationPlayer.play()
+		$AnimationPlayer.play($AnimationPlayer.get_animation_list()[0])
 		already_played = true
